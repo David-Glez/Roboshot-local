d101a18123266c27c8122f9243ac53b4b974e325
QuiqueM <m_r_enrique@outlook.com>
2021-03-01 14:09:07 -0600

    se agrego la funcion para activar las recetas cuando se rellenan ingredientes

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,6 @@ use Illuminate\Support\Facades\Route;
 /*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });*/
-
 // rutas inicio de sesion
 Route::post('/login', 'InicioController@inicioSesion');
 Route::post('/logout', 'InicioController@cerrarSesion');
