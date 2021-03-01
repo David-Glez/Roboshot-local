5cad6ccbe2c1c0b0f1aa6eb4ece96d40b5a6095d
JesusTrejo <jesustrejoo@hotmail.com>
2021-03-01 15:41:24 -0600

    Added try/catch clausule in evento model to prevent malformed eventos from crashing API requests

--- app/Models/Evento.php
+++ app/Models/Evento.php
@@ -35,53 +35,56 @@ class Evento extends Model
 
             $info = json_decode($model->info);
 
-            switch ($model->evento) {
-                case 101: //Ingrediente agregado (dar de alta)
-                    $model->msg = "Se dio de alta el ingrediente {$info->marca}";
-                    break;
-                case 102: //Ingrediente removido (dar de baja)
-                    $model->msg = "Se dio de baja el ingrediente {$info->marca}";
-                    break;
-                case 103: //Ingrediente calibrado (modificar cantidad)
-                    $model->msg = "Se modifico la cantidad del ingrediente {$info->marca} en la posicion {$info->pos}, de {$info->cantidadPrevia}ml a {$info->cantidadNueva}ml";
-                    break;
-                case 104: //Botella agotada (posicion)
-                    $model->msg = "La botella de {$info->marca} en la posicion {$info->pos} se agoto";
-                    break;
-                case 105: //Ingrediente agotado (general)
-                    $model->msg = "El ingrediente {$info->marca} se agoto";
-                    break;
-                case 201: //Receta agregada (dar de alta)
-                    $model->msg = "Se agrego la receta {$info->nombre}";
-                    break;
-                case 202: //Receta removida (dar de baja)
-                    $model->msg = "Se elimino la receta {$info->nombre}";
-                    break;
-                case 401: //Venta
-                    $model->msg = "Se realizo una venta por {$info->total}, id {$info->id}";
-                    break;
-                case 402: //Bebiga entregada en bandeja
-                    $model->msg = "Se entrego una bebida en la bandeja {$info->bandeja}, del pedido {$info->idVenta}";
-                    break;
-                case 403: //Bebida recogida
-                    $model->msg = "Se recogio una bebida de la bandeja {$info->bandeja}, del pedido {$info->idVenta}";
-                    break;
-                case 801: //Calibracion robot SCARA
-                    $model->msg = "El robot de la celda fue exitosamente calibrado";
-                    break;
-                case 901: //Arranque sistema de pedidos
-                    $model->msg = "Arranque del sistema de pedidos RoboShot. Inicializado";
-                    break;
-                case 902: //Cierre sistema de pedidos
-                    $model->msg = "Cierre del sistema de pedidos RoboShot. Desactivando";
-                    break;
-                case 903: //Cierre sistema de pedidos
-                    $model->msg = "Inicio de sesion del usuario {$info->usuario}";
-                    break;
-                case 904: //Cierre sistema de pedidos
-                    $model->msg = "Cierre de sesion del usuario {$info->usuario}";
-                    break;
+            try{
+                switch ($model->evento) {
+                    case 101: //Ingrediente agregado (dar de alta)
+                        $model->msg = "Se dio de alta el ingrediente {$info->marca}";
+                        break;
+                    case 102: //Ingrediente removido (dar de baja)
+                        $model->msg = "Se dio de baja el ingrediente {$info->marca}";
+                        break;
+                    case 103: //Ingrediente calibrado (modificar cantidad)
+                        $model->msg = "Se modifico la cantidad del ingrediente {$info->marca} en la posicion {$info->pos}, de {$info->cantidadPrevia}ml a {$info->cantidadNueva}ml";
+                        break;
+                    case 104: //Botella agotada (posicion)
+                        $model->msg = "La botella de {$info->marca} en la posicion {$info->pos} se agoto";
+                        break;
+                    case 105: //Ingrediente agotado (general)
+                        $model->msg = "El ingrediente {$info->marca} se agoto";
+                        break;
+                    case 201: //Receta agregada (dar de alta)
+                        $model->msg = "Se agrego la receta {$info->nombre}";
+                        break;
+                    case 202: //Receta removida (dar de baja)
+                        $model->msg = "Se elimino la receta {$info->nombre}";
+                        break;
+                    case 301: //Bebiga entregada en bandeja
+                        $model->msg = "Se entrego una bebida en la bandeja {$info->bandeja}, del pedido {$info->idVenta}";
+                        break;
+                    case 302: //Bebida recogida
+                        $model->msg = "Se recogio una bebida de la bandeja {$info->bandeja}, del pedido {$info->idVenta}";
+                        break;
+                    case 801: //Arranque sistema de pedidos
+                        $model->msg = "Arranque del sistema de pedidos RoboShot. Inicializado";
+                        break;
+                    case 802: //Cierre sistema de pedidos
+                        $model->msg = "Cierre del sistema de pedidos RoboShot. Desactivando";
+                        break;
+                    case 803: //Cierre sistema de pedidos
+                        $model->msg = "Inicio de sesion del usuario {$info->usuario}";
+                        break;
+                    case 804: //Cierre sistema de pedidos
+                        $model->msg = "Cierre de sesion del usuario {$info->usuario}";
+                        break;
+                    case 901: //Calibracion robot SCARA
+                        $model->msg = "El robot de la celda fue exitosamente calibrado";
+                        break;
+                }
+            }catch(Exception $ex)
+            {
+                $model->msg = "Invalid event. Please contact technical support. Event ID: {$model->id}";
             }
+
         });
     }   
 }
